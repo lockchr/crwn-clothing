2193fba005358423d455239d8d98b3e7aa39dc84
lockchr <chrislock001@gmail.com>
2022-02-27 13:54:41 -0700

    implmented firebase utils, included ability to store authenticated users into firestore database

--- src/firebase/firebase.utils.js
+++ src/firebase/firebase.utils.js
@@ -1,6 +1,7 @@
-import firebase from 'firebase/compat/app';
-import 'firebase/compat/firestore';
-import 'firebase/compat/auth';
+import firebase from "firebase/compat/app";
+import "firebase/compat/firestore";
+import "firebase/compat/auth";
+
 
 const config = {
   apiKey: "AIzaSyDg2LLsJg2MLQ5R1vLl3crkt_ZlWHMpd10",
@@ -9,6 +10,32 @@ const config = {
   storageBucket: "crwn-db-976ae.appspot.com",
   messagingSenderId: "706275131049",
   appId: "1:706275131049:web:c508a847620912d7c333b8",
+};
+
+export const createUserProfileDocument = async (userAuth, additionalData) => {
+  if (!userAuth) return;
+
+  const userRef = firestore.doc(`users/${userAuth.uid}`);
+
+  const snapShot = await userRef.get();
+
+  if (!snapShot.exists) {
+    const { displayName, email } = userAuth;
+    const createdAt = new Date();
+
+    try {
+      await userRef.set({
+        displayName,
+        email,
+        createdAt,
+        ...additionalData,
+      });
+    } catch (error) {
+      console.log("error creating user.", error.message);
+    }
+  }
+
+  return userRef;
 };
 
 firebase.initializeApp(config);
